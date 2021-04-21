3a902bcbd669cdb4f3c644ac3e5f8d5990be431e
yy96 <chenyy1996@yahoo.com>
2021-04-21 14:31:32 +0800

    Update vegeta_test.sh

--- vegeta_test.sh
+++ vegeta_test.sh
@@ -1,3 +1,4 @@
+# reference: https://www.scaleway.com/en/docs/vegeta-load-testing/
 # create targets.txt with (POST/GET url; Content-Type; @path/to/request.json)
 vegeta attack -duration=5s -rate=5 -targets=targets.txt -output=results.bin
 vegeta plot -title=Results results.bin > results-plot.html
