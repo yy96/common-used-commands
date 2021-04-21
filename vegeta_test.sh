710b4c7fed13b2ae6aa8cf4f6c9aa6fa9386bb86
yy96 <chenyy1996@yahoo.com>
2021-04-21 14:29:21 +0800

    Create vegeta_test.sh

---
+++ vegeta_test.sh
@@ -0,0 +1,4 @@
+# create targets.txt with (POST/GET url; Content-Type; @path/to/request.json)
+vegeta attack -duration=5s -rate=5 -targets=targets.txt -output=results.bin
+vegeta plot -title=Results results.bin > results-plot.html
+vegeta report results.bin
