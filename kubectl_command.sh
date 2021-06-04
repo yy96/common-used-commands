089ad61e2046cf01444d5a6752cc6ec60c030f21
yy96 <chenyy1996@yahoo.com>
2021-06-04 21:50:47 +0800

    Update kubectl_command.sh

--- kubectl_command.sh
+++ kubectl_command.sh
@@ -2,3 +2,6 @@ kubectl get pods --all-namespaces
 kubectl get service --all-namespaces
 
 kubectl port-forward -n feast-0-7-2 service/online-serving 6566:6566
+kubectl get limitrange gke-resource-quotas --namespace=yuanyuan-chen --output=yaml
+# get the file that use the most storage 
+kubectl exec -it --namespace=yuanyuan-chen drift-calibration-test-0 -- du -d 1
